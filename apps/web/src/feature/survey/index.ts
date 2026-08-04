c3078cd31226b1d391763874aac69ba444bb93cc
kyosuke <kyosuke.k.94@gmail.com>
2026-08-05 08:05:06 +0900

    fix(web): 未使用の公開型を削除

--- apps/web/src/feature/survey/index.ts
+++ apps/web/src/feature/survey/index.ts
@@ -1,6 +1,5 @@
 export { SwipeSurvey } from "./presentation/components/swipe-survey";
 export { fetchSurveyDefinition, fetchSurveyList } from "./infrastructure/survey-api";
 export { SurveyDetailError } from "./model/survey-detail-error";
-export type { SurveyDetailErrorReason } from "./model/survey-detail-error";
 export type { SurveyDefinition } from "./model/survey-definition";
 export type { SurveyListItem } from "./model/survey-list-item";
