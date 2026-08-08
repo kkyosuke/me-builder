50ad06e826fd1370ab403d5947ebb4b999968ab6
kyosuke <kyosuke.k.94@gmail.com>
2026-08-09 08:20:14 +0900

    fix(web): featureの未使用exportを削除

--- apps/web/src/feature/admin/index.ts
+++ apps/web/src/feature/admin/index.ts
@@ -1,3 +1,1 @@
 export { default as AdminApplication } from "./presentation/admin-application";
-export { AdminStatisticsScreen } from "./presentation/admin-statistics-screen";
-export { useAdminStatistics } from "./presentation/use-admin-statistics";
--- apps/web/src/feature/profile/index.ts
+++ apps/web/src/feature/profile/index.ts
@@ -1,3 +1,1 @@
 export { default as ProfileApplication } from "./presentation/profile-application";
-export { ProfileSummaryScreen } from "./presentation/profile-summary-screen";
-export { useProfileSummary } from "./presentation/use-profile-summary";
